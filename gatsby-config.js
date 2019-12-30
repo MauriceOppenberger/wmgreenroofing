0ab4c608d0d63e4e7fe64eb8d7f603f991146335
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2019-12-30 13:51:31 -0500

    delcare process env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,8 +40,8 @@ module.exports = {
     {
       resolve: "gatsby-wpgraphql-inline-images",
       options: {
-        wordPressUrl: "https://wordpress.oppenberger.com/",
-        uploadsUrl: "https://wordpress.oppenberger.com/wp-content/uploads/",
+        wordPressUrl: `${process.env.API_PROTOCOL}://${API_URL}/`,
+        uploadsUrl: `${process.env.API_PROTOCOL}://${API_URL}/wp-content/uploads/`,
         processPostTypes: ["Page", "Post", "CustomPost"],
         graphqlTypeName: "WPGraphQL",
         generateWebp: true,
@@ -55,7 +55,7 @@ module.exports = {
         // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
         fieldName: "wpgraphql",
         // Url to query from
-        url: "https://wordpress.oppenberger.com/graphql",
+        url: `${process.env.API_PROTOCOL}://${API_URL}/graphql`,
       },
     },
     {
