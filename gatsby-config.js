ec721a896347eb6d588583afff39baabe8bc9074
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2019-12-30 14:00:12 -0500

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+require((id = "dotenv")).config({
+  path: ".env",
+})
+const path = require("path")
+
 module.exports = {
   siteMetadata: {
     title: `Wm. Green Roofing Ldt.`,
@@ -13,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: path.join(__dirname, "src", "images"),
       },
     },
     `gatsby-transformer-sharp`,
@@ -84,5 +89,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
   ],
 }
