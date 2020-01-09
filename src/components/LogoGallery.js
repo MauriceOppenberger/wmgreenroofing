519db007bcc4eef18349ce6a8090c8023b64fab7
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2020-01-09 14:13:56 -0500

    merged with branch preview and updated css

--- src/components/LogoGallery.js
+++ src/components/LogoGallery.js
@@ -1,31 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import styled from "styled-components"
+import LogoGalleryWrapper from "./styles/LogoGalleryStyles"
 
-const LogoGalleryWrapper = styled.div`
-  margin: 2rem;
-  h2.title {
-    text-align: center;
-  }
-  .logo__gallery {
-    margin: 0 auto;
-    display: flex;
-    flex-flow: row wrap;
-    align-items: center;
-    justify-content: center;
-    max-width: 1280px;
-    padding: 1rem 0;
-  }
-  .gallery__item {
-    list-style: none;
-    flex: 1;
-    max-width: 250px;
-    min-width: 100px;
-    margin: 1rem 2rem;
-    padding: 1rem 0;
-  }
-`
 const LogoGallery = () => {
   const {
     wpgraphql: {
