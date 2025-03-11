0496dde4ec9d74da42beee79cb5853aafeaa084e
Maurice Oppenberger <oppenqma@amazon.com>
2025-03-11 16:48:17 -0400

    Revert "Remove social feed for testing"
    This reverts commit 9b45b12b692f40b1673643d296ef30645e9e9040.

--- src/templates/homepage.js
+++ src/templates/homepage.js
@@ -7,7 +7,7 @@ import contentParser from "gatsby-wpgraphql-inline-images"
 import TestemonySlider from "../components/TestemonySlider"
 import ContentWrapper from "./templateStyles/HomePageStyle"
 import SEO from "../components/Seo"
-// import Instagram from "../components/instagram"
+import Instagram from "../components/instagram"
 
 const HomepageTemplate = props => {
   const {
@@ -17,6 +17,8 @@ const HomepageTemplate = props => {
     pageContext: { pluginOptions },
   } = props
 
+  
+ 
   const { title, content, description } = currentPage
   return (
     <Layout>
@@ -31,10 +33,10 @@ const HomepageTemplate = props => {
           <h2 className="title">Reviews</h2>
           <TestemonySlider />
         </div>
-        {/* <div className="socialMedia__container">
+        <div className="socialMedia__container">
           <h2 className="title">Recent Projects</h2>
           <Instagram />
-        </div> */}
+        </div>
         <LogoGallery />
       </ContentWrapper>
     </Layout>
